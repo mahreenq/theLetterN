fdee176d6068bcab1b75144e8e86bf7b6a105b67
Mahreen Qureshi <mahreenq@hotmail.com>
2020-02-22 22:28:20 -0500

    additions to animation

---
+++ index.js
@@ -0,0 +1,133 @@
+// function addCircles() {
+//   for (i = 0; i < 18; i++) {
+//     var circle1 = "<div id='" + [i + 1] + " ' class='ball red'></div>";
+//     var circle2 = "<div id='" + [i + 19] + " ' class='ball red'></div>";
+//     var circle3 = "<div id='" + [i + 37] + " ' class='ball green '></div>";
+//     var circle4 = "<div id='" + [i + 55] + " ' class='ball blue'></div>";
+
+//     $(".letterN").after(circle1, circle2, circle3, circle4);
+//   }
+// }
+
+var randomCircle = Math.floor(Math.random() * 71) + 1;
+console.log(randomCircle);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("1");
+  element.classList.add("active");
+}, 1000);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("2");
+  element.classList.add("active");
+}, 1200);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("3");
+  element.classList.add("active");
+}, 1400);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("4");
+  element.classList.add("active");
+}, 1600);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("5");
+  element.classList.add("active");
+}, 1800);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("6");
+  element.classList.add("active");
+}, 2000);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("7");
+  element.classList.add("active");
+}, 2200);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("8");
+  element.classList.add("active");
+}, 2400);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("8");
+  element.classList.add("active");
+}, 2600);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("9");
+  element.classList.add("active");
+}, 2800);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("10");
+  element.classList.add("active");
+}, 3000);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("11");
+  element.classList.add("active");
+}, 3200);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("12");
+  element.classList.add("active");
+}, 3400);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("13");
+  element.classList.add("active");
+}, 3600);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("14");
+  element.classList.add("active");
+}, 3800);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("15");
+  element.classList.add("active");
+}, 4000);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("16");
+  element.classList.add("active");
+}, 4200);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("17");
+  element.classList.add("active");
+}, 4400);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("18");
+  element.classList.add("active");
+}, 4600);
+
+window.setTimeout(function() {
+  //   console.log(randomCircle);
+  var element = document.getElementById("19");
+  element.classList.add("active");
+}, 4800);
